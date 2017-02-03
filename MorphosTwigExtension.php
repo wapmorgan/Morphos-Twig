aece0afe4d2d060816e960334339e7eb4b18fc9e
wapmorgan <wapmorgan@gmail.com>
2017-02-04 01:00:59 +0300

    Update extension

--- MorphosTwigExtension.php
+++ MorphosTwigExtension.php
@@ -1,18 +1,31 @@
 <?php
 namespace morphos;
 
+use morphos\NumeralCreation;
+use morphos\Russian\CardinalNumeral;
 use morphos\Russian\Plurality;
 
 class MorphosTwigExtension extends \Twig_Extension {
     public function getFilters() {
         return array(
             new \Twig_SimpleFilter('plural', array($this, 'pluralFilter')),
+            new \Twig_SimpleFilter('numeral', array($this, 'numeralFilter')),
             new \Twig_SimpleFilter('name', array($this, 'nameFilter')),
         );
     }
 
     public function pluralFilter($word, $count) {
         return $count.' '.Plurality::pluralize($word, $count);
+    }
+
+    public function numeralFilter($word, $count = null, $gender = NumeralCreation::MALE) {
+        if ($count === null) {
+            return CardinalNumeral::generate($word);
+        } else if (in_array($count, array('m', 'f', 'n'))) {
+            return CardinalNumeral::generate($word, $count);
+        } else {
+            return CardinalNumeral::generate($count, $gender).' '.Plurality::pluralize($word, $count);
+        }
     }
 
     public function nameFilter($name, $gender = null, $case = null) {

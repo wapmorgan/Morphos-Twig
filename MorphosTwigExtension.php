af46e04673b3f6e3b0e058dfe24d635e041c3b24
wapmorgan <wapmorgan@gmail.com>
2017-08-16 00:00:53 +0300

    Update Morphos version

--- MorphosTwigExtension.php
+++ MorphosTwigExtension.php
@@ -1,10 +1,9 @@
 <?php
 namespace morphos;
 
-use morphos\Cases;
-use morphos\NumeralCreation;
-use morphos\Russian\CardinalNumeral;
-use morphos\Russian\OrdinalNumeral;
+use morphos\Russian\CardinalNumeralGenerator;
+use morphos\Russian\MoneySpeller;
+use morphos\Russian\OrdinalNumeralGenerator;
 use morphos\Russian\Plurality;
 
 class MorphosTwigExtension extends \Twig_Extension {
@@ -19,68 +18,32 @@ class MorphosTwigExtension extends \Twig_Extension {
     }
 
     public function pluralFilter($word, $count) {
-        return $count.' '.Plurality::pluralize($word, $count);
+        return \morphos\Russian\pluralize($count, $word);
     }
 
     public function moneyFilter($value, $currency) {
-        $money_big = floor($value);
-        $money_little = floor($value * 100 % 100);
-        switch ($currency) {
-            case '₽':
-            case 'р':
-            case 'рубль':
-            case 'r':
-            case 'rub':
-                return $money_big.' '.Plurality::pluralize('рубль', $money_big).' '.$money_little.' '.Plurality::pluralize('копейка', $money_little);
-
-            case '₴':
-            case 'г':
-            case 'гривна':
-            case 'uah':
-                return $money_big.' '.Plurality::pluralize('гривна', $money_big).' '.$money_little.' '.Plurality::pluralize('копейка', $money_little);
-
-            case '$':
-            case 'доллар':
-            case 'u':
-            case 'usd':
-                return $money_big.' '.Plurality::pluralize('доллар', $money_big).' '.$money_little.' '.Plurality::pluralize('цент', $money_little);
-
-            case '€':
-            case 'евро':
-            case 'e':
-            case 'eur':
-            case 'euro':
-                return $money_big.' '.Plurality::pluralize('евро', $money_big).' '.$money_little.' '.Plurality::pluralize('цент', $money_little);
-
-            case '£':
-            case 'фунт':
-            case 'gbp':
-                return $money_big.' '.Plurality::pluralize('фунт', $money_big).' '.$money_little.' '.Plurality::pluralize('пенни', $money_little);
-
-            default:
-                return $expression[0].' '.$expression[1];
-        }
+        return MoneySpeller::spell($value, $currency);
     }
 
-    public function numeralFilter($word, $count = null, $gender = NumeralCreation::MALE) {
+    public function numeralFilter($word, $count = null, $gender = Gender::MALE) {
         if ($count === null) {
-            return CardinalNumeral::getCase($word, Cases::NOMINATIVE);
+            return CardinalNumeralGenerator::getCase($word, Cases::NOMINATIVE);
         } else if (in_array($count, array('m', 'f', 'n'))) {
-            return CardinalNumeral::getCase($word, Cases::NOMINATIVE, $count);
+            return CardinalNumeralGenerator::getCase($word, Cases::NOMINATIVE, $count);
         } else {
-            return CardinalNumeral::getCase($count, Cases::NOMINATIVE, $gender).' '.Plurality::pluralize($word, $count);
+            return CardinalNumeralGenerator::getCase($count, Cases::NOMINATIVE, $gender).' '.Plurality::pluralize($word, $count);
         }
     }
 
-    public function ordinalFilter($number, $gender = NumeralCreation::MALE) {
-        return OrdinalNumeral::getCase($number, Cases::NOMINATIVE, $gender);
+    public function ordinalFilter($number, $gender = Gender::MALE) {
+        return OrdinalNumeralGenerator::getCase($number, Cases::NOMINATIVE, $gender);
     }
 
     public function nameFilter($name, $gender = null, $case = null) {
         if ($case === null)
-            return \morphos\Russian\name($name, $gender);
+            return \morphos\Russian\inflectName($name, $gender);
         else
-            return \morphos\Russian\name($name, $case, $gender);
+            return \morphos\Russian\inflectName($name, $case, $gender);
     }
 
     public function getName() {
